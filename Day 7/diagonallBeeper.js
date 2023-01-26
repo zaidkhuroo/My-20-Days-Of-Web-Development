8279cdc53c89cc6f1c7893fc8de9319e5c11aa03
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-01-26 12:57:52 +0530

    Add files via upload

---
+++ Day 7/diagonallBeeper.js
@@ -0,0 +1,21 @@
+// Exercise to put beeper diagonaly in 5*5 matrix
+
+function main(){
+    putBeeper();
+    move();
+    getItUp();
+   getItUp();
+   getItUp();
+    getItUp();
+ }
+ function getItUp()
+ {
+   
+    //move();
+    turnLeft();
+    move();
+    putBeeper();
+    turnRight();
+    move();
+   
+ }

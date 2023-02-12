b3f4e6b35cffce57b9fca371dc7ffd8ed46867c5
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-02-12 12:21:09 +0530

    Add files via upload

---
+++ Day 08/array.js
@@ -0,0 +1,11 @@
+ var b=["zaid", "bin", "farooq"];
+var a=prompt("Enter your name");
+if (a=="zaid" || a=="bin" || a=="farooq") //this logic can also be used
+if (b.includes(a)) //function used to identify element in the array.
+{
+alert("Welcome Zaid");
+    }
+else
+{
+alert("get out");
+    }
---
+++ Day 08/fibb.js
@@ -0,0 +1,13 @@
+var start=[];
+var count=0;
+var i=0;
+function fib(number){
+for(i=0;i<=20;i++)
+    {
+       var count =number+number-2;
+        start.push(count);
+        count++;
+    
+console.log(start);
+}
+}

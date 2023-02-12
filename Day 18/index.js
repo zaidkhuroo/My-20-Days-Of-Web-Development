15f12833919b543238d5c170f16cdf89d334eab9
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-02-12 21:11:53 +0530

    Delete Day 18 directory

--- Day 18/index.js
+++
@@ -1,9 +0,0 @@
-$(".nav-link").click(function(){
-    var audio = new Audio("/Sound/Click.mp3"); //making a varibale of audio and setting path of the sounds where the audio file is placed.
-    audio.play();
-});
-
-$(".navbar-brand").hover(function(event){
-    var sword=new Audio("/Sound/sword.mp3");
-    sword.play(); //stuff to do on mouseover
-});

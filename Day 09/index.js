dc1ee0cac3b9f3479dd2545fa8b4ddabfe359d57
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-02-12 20:40:47 +0530

    Add files via upload

---
+++ Day 09/index.js
@@ -0,0 +1,4 @@
+document.query
+document.querySelector("h1").classList.add("huge"); //this query when used will append with the class huge made in css and will change the size of heading.
+
+document.querySelector("li a").style.color="red"; //this query when used will change the color of first list element to red.

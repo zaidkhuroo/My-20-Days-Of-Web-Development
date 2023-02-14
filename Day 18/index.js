c089ec536534b1c9c3cd7466a629516654913beb
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-02-14 23:53:34 +0530

    Update index.js
    removed bigB class and added methods to create animation of h1 and h2 tags using Typed method.

--- Day 18/index.js
+++ Day 18/index.js
@@ -7,8 +7,31 @@ $(".navbar-brand").hover(function(event){
     var sword=new Audio("/Sound/sword.mp3");
     sword.play(); //stuff to do on mouseover
 });
-$("h1").addClass("bigB");
-$("h1").on("mouseover", function () { //on takes two parameters 1.event you're looking to listen for 2. call back function is the second parameter which will perform the action.
-    $("h1").css("color", "skyblue"); //now as the mouseover is detected on h1, it will change the color to skyblue.
+// $("h1").addClass("bigB");
+// $("h1").on("mouseover", function () { //on takes two parameters 1.event you're looking to listen for 2. call back function is the second parameter which will perform the action.
+//     $("h1").css("color", "skyblue"); //now as the mouseover is detected on h1, it will change the color to skyblue.
   
+// });
+var typed = new Typed(".new-text", {
+    strings:[" Zaid", "a Programmer",  "a Photographer","a Stargazer ★"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+    
 });
+var type = new Typed(".new-era", {
+    strings:[" About me"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+    
+});
+var types = new Typed(".new-eras", {
+    strings:[" My Work"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+    
+});
+// changing the color of main text using jquery.
+$(".new-text").css("color","yellow");

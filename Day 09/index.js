fee71b37212939a549bd5b772105a9a1acec2984
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-03-06 16:20:37 +0530

    Update index.js

--- Day 09/index.js
+++ Day 09/index.js
@@ -1,4 +1,4 @@
 document.query
-document.querySelector("h1").classList.add("huge"); //this query when used will append with the class huge made in css and will change the size of heading.
+document.querySelector("h1").classList.add("huge"); //This query when used will append with the class huge made in css and will change the size of heading.
 
-document.querySelector("li a").style.color="red"; //this query when used will change the color of first list element to red.
+document.querySelector("li a").style.color="red"; //This query when used will change the color of first list element to red.

c647350d37f541182c4307b17e178da16c1ca687
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-04-02 12:40:20 +0530

    Update leapYear.js

--- Day 08/leapYear.js
+++ Day 08/leapYear.js
@@ -1,4 +1,6 @@
-    var a=prompt("Enter year");
+// adding varibale in javascript which will display enter year    
+var a=prompt("Enter year");
+// if anything divided by 4 is 0 then it will display leap year
     if(a% 4===0 && a%100===0 &&  a%400===0) 
     {
         alert("Leap year");

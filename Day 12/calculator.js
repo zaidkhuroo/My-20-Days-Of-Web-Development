d3c395027288cd12ce6e145198fc60833b41f307
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-04-04 13:27:38 +0530

    Update calculator.js

--- Day 12/calculator.js
+++ Day 12/calculator.js
@@ -1,15 +1,20 @@
+// function to add
 function add(a,b){
     return a+b;
 }
+// function to subtract
 function sub(a,b){
     return a-b;
 }
+// function to multiply
 function mul(a,b){
     return a*b;
 }
+// function to divide
 function divide(a,b){
     return a/b;
 }
+// function to choose two operators and operating on them
 function calculator(a,b,operator){
     return operator(a,b);
 }

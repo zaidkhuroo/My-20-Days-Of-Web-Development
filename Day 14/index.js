b820d4b788a9c6836f64cb664276d30b5b2bee36
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-10-03 23:33:56 +0530

    Update index.js

--- Day 14/index.js
+++ Day 14/index.js
@@ -1,4 +1,4 @@
 $("body").keypress(function (event) { 
     $("h1").text("Level 1");
 }); //writing a query which will take the whole body of html page and listening to the keypress from the keyboard which will be appended with anonymous function, in which we'll change the text of h1 to level 1 using .text method.
-
+ 
